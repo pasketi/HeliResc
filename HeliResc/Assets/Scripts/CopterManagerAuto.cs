a24c785b69756b83a59a15ef5301fda24b1546c1
Pasketi <pasketi@gmail.com>
2015-03-12 14:57:29 +0200

    WIP

---
+++ HeliResc/Assets/Scripts/CopterManagerAuto.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CopterManagerAuto : MonoBehaviour {
+
+	private Rigidbody2D copterBody;
+	private DistanceJoint2D hookJoint;
+	private bool isHookDown = true, once = false;
+
+	public GameObject hookPrefab, hookAnchor;
+	private GameObject hook;
+	public float 	X = 25f, 
+					tiltSpeed = 100f, 
+					returnSpeed = 5f, 
+					power = 20f,
+					flyingAltitude = 4f, 
+					maxX = 15f,
+					hookDistance = 1.5f;
+
+// Use this for initialization
+	void Start () {
+		copterBody = gameObject.GetComponent<Rigidbody2D>();
+		hookJoint = GetComponent<DistanceJoint2D> ();
+		hookJoint.anchor = hookAnchor.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Helicopter turn to X degrees
+		if (gameObject.transform.eulerAngles.z != X) { // Turn to currentAngle
+			if (X < 180f) {
+				if (gameObject.transform.eulerAngles.z > 180f) {
+					// Rotate CCW
+					gameObject.transform.Rotate(new Vector3(0f, 0f, tiltSpeed * Time.deltaTime));
+				} else if (gameObject.transform.eulerAngles.z < 180f) {
+					if (gameObject.transform.eulerAngles.z < X){
+						// Rotate CCW
+						gameObject.transform.Rotate (new Vector3 (0f, 0f, tiltSpeed * Time.deltaTime));
+					} else if (gameObject.transform.eulerAngles.z > X) {
+						// Rotate CW
+						gameObject.transform.Rotate(new Vector3(0f, 0f, -tiltSpeed * Time.deltaTime));
+					}
+				}
+			} else if (X > 180f) {
+				if (gameObject.transform.eulerAngles.z < 180f) {
+					// Rotate CW
+					gameObject.transform.Rotate (new Vector3 (0f, 0f, -tiltSpeed * Time.deltaTime));
+				} else if (gameObject.transform.eulerAngles.z > 180f) {
+					if (gameObject.transform.eulerAngles.z < X) {
+						// Rotate CCW
+						gameObject.transform.Rotate (new Vector3 (0f, 0f, tiltSpeed * Time.deltaTime));
+					} else if (gameObject.transform.eulerAngles.z > X) {
+						// Rotate CW
+						gameObject.transform.Rotate(new Vector3(0f, 0f, -tiltSpeed * Time.deltaTime));
+					}
+				}
+			}
+		}
+		
+		// Automatic 
+		if (gameObject.transform.position.y < flyingAltitude) {
+			copterBody.AddForce (gameObject.transform.up * (power*100) * Time.deltaTime);
+		} else if (gameObject.transform.position.y > flyingAltitude) {
+			copterBody.AddForce ((gameObject.transform.up - new Vector3(0f, gameObject.transform.up.y, 0f)) * (power*100) * Time.deltaTime);
+		}
+
+		if (isHookDown && hook == null) {
+			once = true;
+			hook = Instantiate (hookPrefab, gameObject.transform.position + new Vector3 (0f, -0.3f), Quaternion.identity) as GameObject;
+			hookJoint.enabled = true;
+			hookJoint.distance = hookDistance;
+			hookJoint.connectedBody = hook.GetComponent<Rigidbody2D>();
+		}
+
+		if (gameObject.transform.position.x >= maxX) {
+			gameObject.transform.position = new Vector3(-(gameObject.transform.position.x), gameObject.transform.position.y+2f);
+			hook.transform.position = new Vector3(-(hook.transform.position.x), hook.transform.position.y);
+		}
+	}
+}
--- HeliResc/Assets/Scripts/CrateManager.cs
+++ HeliResc/Assets/Scripts/CrateManager.cs
@@ -16,75 +16,92 @@ public class CrateManager : MonoBehaviour {
 	public float inWaterModifier = 0.2f;
 
 	public Sprite Dropped, Hooked, DroppedBG, HookedBG;
-	public bool inWater = false, inCargo = false;
+	public bool inWater = false, inCargo = false, inMenu = false;
 
 
 
 	// Use this for initialization
 	void Start () {
-		Time.timeScale = 1f;
-		manager = GameObject.Find("LevelManagerO").GetComponent<LevelManager>();
-		copterScript = GameObject.Find ("Copter").GetComponent<CopterManagerTouch>();
-		cargoScript = GameObject.Find ("Copter").GetComponent<CargoManager>();
+		if (!inMenu) {
+			Time.timeScale = 1f;
+			manager = GameObject.Find ("LevelManagerO").GetComponent<LevelManager> ();
+			copterScript = GameObject.Find ("Copter").GetComponent<CopterManagerTouch> ();
+			cargoScript = GameObject.Find ("Copter").GetComponent<CargoManager> ();
+		}
 		crate = gameObject.transform.parent.gameObject;
-		spriteRenderer = crate.GetComponent<SpriteRenderer>();
-		if (crate.transform.FindChild("BackGround") != null) bgRenderer = crate.transform.FindChild("BackGround").GetComponent<SpriteRenderer>();
-		floatyValue = gameObject.transform.parent.GetComponent<Rigidbody2D>().mass * 30f;
-		hinge = GetComponent<HingeJoint2D>();
+		spriteRenderer = crate.GetComponent<SpriteRenderer> ();
+		if (crate.transform.FindChild ("BackGround") != null)
+			bgRenderer = crate.transform.FindChild ("BackGround").GetComponent<SpriteRenderer> ();
+		floatyValue = gameObject.transform.parent.GetComponent<Rigidbody2D> ().mass * 30f;
+		hinge = GetComponent<HingeJoint2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if ((!inCargo && copterScript != null && crate.layer == 11 && copterScript.isHookDead == true) || copterScript == null) {
-			Destroy(joint);
-			cargoScript.changeHookMass (-crateMass);
-			gameObject.GetComponent<Collider2D>().enabled = true;
-			crate.layer = 10;
-			gameObject.transform.parent.parent = null;
-		}
-
-		if (crate != null && (!inCargo && crate.transform.position.y < manager.getWaterLevel ())) {
-			if (manager.getPaused() == false) crate.GetComponent<Rigidbody2D>().AddForce (Vector3.up * floatyValue);
-			inWater = true;
-		} else {
-			inWater = false;
-		}
-
-		if (crate != null && copterScript != null && crate.layer == 11 && inWater != lastInWater) {
-			lastInWater = inWater;
-			if (inWater && !inCargo) {
-				cargoScript.changeHookMass ((-crateMass) + (crateMass*inWaterModifier));
-			} else if (!inWater || inCargo) {
-				cargoScript.changeHookMass ((crateMass) + -(crateMass*inWaterModifier));
+		if (!inMenu) {
+			if ((!inCargo && copterScript != null && crate.layer == 11 && copterScript.isHookDead == true) || copterScript == null) {
+				Destroy (joint);
+				cargoScript.changeHookMass (-crateMass);
+				gameObject.GetComponent<Collider2D> ().enabled = true;
+				crate.layer = 10;
+				gameObject.transform.parent.parent = null;
 			}
-		}
 
-		if (crate.layer == 11) {
-			spriteRenderer.sprite = Hooked;
-			if (bgRenderer != null) bgRenderer.sprite = HookedBG;
+			if (crate != null && (!inCargo && crate.transform.position.y < manager.getWaterLevel ())) {
+				if (manager.getPaused () == false)
+					crate.GetComponent<Rigidbody2D> ().AddForce (Vector3.up * floatyValue);
+				inWater = true;
+			} else {
+				inWater = false;
+			}
+
+			if (crate != null && copterScript != null && crate.layer == 11 && inWater != lastInWater) {
+				lastInWater = inWater;
+				if (inWater && !inCargo) {
+					cargoScript.changeHookMass ((-crateMass) + (crateMass * inWaterModifier));
+				} else if (!inWater || inCargo) {
+					cargoScript.changeHookMass ((crateMass) + -(crateMass * inWaterModifier));
+				}
+			}
+
+			if (crate.layer == 11) {
+				spriteRenderer.sprite = Hooked;
+				if (bgRenderer != null)
+					bgRenderer.sprite = HookedBG;
+			} else {
+				spriteRenderer.sprite = Dropped;
+				if (bgRenderer != null)
+					bgRenderer.sprite = DroppedBG;
+			}
 		} else {
-			spriteRenderer.sprite = Dropped;
-			if (bgRenderer != null) bgRenderer.sprite = DroppedBG;
+			if (crate.transform.position.y < 0f) {
+				crate.GetComponent<Rigidbody2D> ().AddForce (Vector3.up * floatyValue);
+				inWater = true;
+			} else {
+				inWater = false;
+			}
 		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-		if (copterScript != null && collision.collider.gameObject.CompareTag ("Hook") && copterScript.isHookDead == false) {
-			gameObject.transform.parent.parent = collision.collider.gameObject.transform;
-			gameObject.AddComponent <DistanceJoint2D>();
+		if (!inMenu) {
+			if (copterScript != null && collision.collider.gameObject.CompareTag ("Hook") && copterScript.isHookDead == false) {
+				gameObject.transform.parent.parent = collision.collider.gameObject.transform;
+				gameObject.AddComponent <DistanceJoint2D> ();
 
-			joint = GetComponent<DistanceJoint2D> ();
+				joint = GetComponent<DistanceJoint2D> ();
 
 
-			joint.connectedBody = collision.collider.gameObject.GetComponent<Rigidbody2D>();
-			joint.connectedAnchor = new Vector2 (0f, -0.3f);
-			joint.distance = 0f;
-			joint.maxDistanceOnly = false;
-			hinge.useLimits = false;
+				joint.connectedBody = collision.collider.gameObject.GetComponent<Rigidbody2D> ();
+				joint.connectedAnchor = new Vector2 (0f, -0.3f);
+				joint.distance = 0f;
+				joint.maxDistanceOnly = false;
+				hinge.useLimits = false;
 
-			gameObject.GetComponent<Collider2D>().enabled = false;
-			crate.layer = 11; //liftedCrate
-			cargoScript.changeHookMass (crateMass);
+				gameObject.GetComponent<Collider2D> ().enabled = false;
+				crate.layer = 11; //liftedCrate
+				cargoScript.changeHookMass (crateMass);
+			}
 		}
 	}
 }
