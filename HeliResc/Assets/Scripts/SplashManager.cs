74bcc839ad1c1c8d6df26cca8e64ec6133146dd4
Pasketi <pasketi@gmail.com>
2015-03-20 13:16:18 +0200

    Splash!

---
+++ HeliResc/Assets/Scripts/SplashManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashManager : MonoBehaviour {
+
+	public float splashTime = 3f, tempTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tempTime += Time.deltaTime;
+		if (tempTime >= splashTime) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
