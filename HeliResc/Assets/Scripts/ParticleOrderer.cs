a5ffd6af23c6ddabccd1c262e7fa492903da8d51
Pasketi <pasketi@gmail.com>
2015-03-25 01:15:13 +0200

    More Forest stuff

---
+++ HeliResc/Assets/Scripts/ParticleOrderer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(ParticleSystem))]
+
+public class ParticleOrderer : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer> ();
+		ParticleSystem particleSystem = GetComponent<ParticleSystem> ();
+		particleSystem.GetComponent<Renderer>().sortingLayerID = spriteRenderer.sortingLayerID;
+		particleSystem.GetComponent<Renderer>().sortingOrder = spriteRenderer.sortingOrder;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
