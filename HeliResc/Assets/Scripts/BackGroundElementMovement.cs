59f070da8f77a7dd5eb9b21247589da6b27c1201
Pasketi <pasketi@gmail.com>
2015-03-27 02:56:35 +0200

    "Late update"... Fucking puns...

--- HeliResc/Assets/Scripts/BackGroundElementMovement.cs
+++ HeliResc/Assets/Scripts/BackGroundElementMovement.cs
@@ -12,7 +12,7 @@ public class BackGroundElementMovement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
 		deltaPos = new Vector3(Camera.main.transform.position.x - lastCamPos.x, Camera.main.transform.position.y - lastCamPos.y);
 		transform.position = new Vector3 (transform.position.x + (deltaPos.x*speedInPercentX), transform.position.y + (deltaPos.y*speedInPercentY));
 		lastCamPos = Camera.main.transform.position;
