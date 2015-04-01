15be4bfb4c4f4fc463f544e314cf5811f4c1810c
Pasketi <pasketi@gmail.com>
2015-04-01 15:59:20 +0300

    Pelikaani fiksattu

--- HeliResc/Assets/Scripts/FlyingObstacleManager.cs
+++ HeliResc/Assets/Scripts/FlyingObstacleManager.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class FlyingObstacleManager : MonoBehaviour {
 
+	private LevelManager manager;
 	private float tempPatrol = 0f;
 	public float speed = 0.2f, patrolTime = 3f;
 	public bool movingRight = true, patrolling = true;
 
 	// Use this for initialization
 	void Start () {
-
+		manager = GameObject.Find ("LevelManagerO").GetComponent<LevelManager> ();
 	}
 	
 	// Update is called once per frame
@@ -22,12 +23,12 @@ public class FlyingObstacleManager : MonoBehaviour {
 			}
 		}
 
-		if (movingRight) {
+		if (movingRight && !manager.getPaused()) {
 			gameObject.transform.localScale = new Vector3(1f, 1f);
-			gameObject.transform.Translate(new Vector3(speed, 0f));
-		} else {
+			gameObject.transform.Translate(new Vector3(speed * Time.deltaTime, 0f));
+		} else if (!movingRight && !manager.getPaused()) {
 			gameObject.transform.localScale = new Vector3(-1f, 1f);
-			gameObject.transform.Translate(new Vector3(-speed, 0f));
+			gameObject.transform.Translate(new Vector3(-speed * Time.deltaTime, 0f));
 		}
 	}
 }
