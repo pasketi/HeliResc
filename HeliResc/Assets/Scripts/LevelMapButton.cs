758fb55f756d771ad980ce90c355e20aaaa8e053
jesseve <jesseverainen@gmail.com>
2015-04-13 22:30:03 +0300

    Fixes to levelmap

--- HeliResc/Assets/Scripts/LevelMapButton.cs
+++ HeliResc/Assets/Scripts/LevelMapButton.cs
@@ -4,64 +4,66 @@ using System.Collections;
 
 public class LevelMapButton : MonoBehaviour {
 
+    //Index of the level. First is 1 etc
 	public int levelIndex;
 
+    //Determines if the level can be entered
 	public bool levelLocked;
 
+    //DELETE ?
 	public string levelName;
 
+    //The star images on top of button
 	public Image[] stars;
 
+    //Sprites to assign to the images
 	public Sprite starUnlocked;
 	public Sprite starLocked;
 
-	private string saveName;
+    //Contain all info of the level to be saved
+    private LevelInfo levelInfo;
 
+    //DELETE?
 	[Range(0, 3)]
 	public int starsAwarded;
 
+    //The sprites on the button
+    public Sprite buttonLocked;
+    public Sprite buttonUnlocked;
+
+    //Changes depending if the level is locked
+    public Image buttonImage;
+
+    //The text in the middle of the button
 	public Text buttonText;
 
 	// Use this for initialization
 	void Start () {
-		saveName = "Level" + levelIndex.ToString();
+        levelInfo = SaveLoad.LoadLevelInfo(levelIndex);
 
-		buttonText.text = levelIndex.ToString ();
+        stars[0].sprite = levelInfo.star1 ? starUnlocked : starLocked;
+        stars[1].sprite = levelInfo.star2 ? starUnlocked : starLocked;
+        stars[2].sprite = levelInfo.star3 ? starUnlocked : starLocked;
 
-		if(PlayerPrefs.HasKey(saveName + "Locked"))
-			levelLocked = PlayerPrefsExt.GetBool(saveName + "Locked");
-		else {
-			PlayerPrefsExt.SetBool(saveName + "Locked", levelIndex > 1);
-		}
+        for (int i = 0; i < stars.Length; i++) {
+            stars[i].enabled = !levelInfo.locked;         
+        }
 
-		if(PlayerPrefs.HasKey(saveName + "Stars")) {
-			starsAwarded = PlayerPrefs.GetInt(saveName + "Stars");
-		}
-		else
-			SetStars(0);
+        if (levelInfo.locked)
+        {
+            buttonImage.sprite = buttonLocked;
+            buttonText.text = "";
+        }
+        else {
+            buttonImage.sprite = buttonUnlocked;
+            buttonText.text = levelIndex.ToString();
+        }
 
-		for(int i = 0; i < stars.Length; i++) {
-			if(!levelLocked) {
-				stars[i].sprite = i < starsAwarded ? starUnlocked : starLocked;
-			}
-			else {
-				stars[i].sprite = starLocked;
-			}
-		}
-	}
-
-	public void SetLocked(bool locked) {
-		PlayerPrefsExt.SetBool(saveName + "Locked", locked);
-		levelLocked = locked;
-	}
-
-	public void SetStars(int stars) {
-		PlayerPrefs.SetInt(saveName + "Stars", stars);
-		starsAwarded = stars;
+		
 	}
 
 	public void StartLevel() {
 		if(!levelLocked) 
-			Application.LoadLevel(saveName);
+			Application.LoadLevel("Level" + levelIndex.ToString());
 	}
 }
--- HeliResc/Assets/Scripts/SaveLoad.cs
+++ HeliResc/Assets/Scripts/SaveLoad.cs
@@ -1,61 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public static class SaveLoad {
 
 	public static void SaveLevelInfo(LevelInfo info) {
-		/*
 
-		if(PlayerPrefs.HasKey(saveName + "Locked"))
-			levelLocked = PlayerPrefsExt.GetBool(saveName + "Locked");
-		else {
-			PlayerPrefsExt.SetBool(saveName + "Locked", levelIndex > 1);
-		}
+        string name = "Level" + info.index.ToString();
 
-		if(PlayerPrefs.HasKey(saveName + "Stars")) {
-			starsAwarded = PlayerPrefs.GetInt(saveName + "Stars");
-		}
-		else
-			SetStars(0);
-
-		for(int i = 0; i < stars.Length; i++) {
-			if(!levelLocked) {
-				stars[i].sprite = i < starsAwarded ? starUnlocked : starLocked;
-			}
-			else {
-				stars[i].sprite = starLocked;
-			}
-		}
-
-		public void SetLocked(bool locked) {
-			PlayerPrefsExt.SetBool(saveName + "Locked", locked);
-			levelLocked = locked;
-		}
-
-		public void SetStars(int stars) {
-			PlayerPrefs.SetInt(saveName + "Stars", stars);
-			starsAwarded = stars;
-		}
-
-		 */
+        PlayerPrefsExt.SetBool(name + "Locked", info.locked);
+        PlayerPrefsExt.SetBool(name + "Star1", info.star1);
+        PlayerPrefsExt.SetBool(name + "Star2", info.star2);
+        PlayerPrefsExt.SetBool(name + "Star3", info.star3);
 	}
 
-	public static LevelInfo LoadLevelInfo(string name) {
-		LevelInfo info = new LevelInfo ();
+	public static LevelInfo LoadLevelInfo(int index) {
+        string level = "Level" + index.ToString();
+        bool locked = false;
+        bool s1 = false;
+        bool s2 = false;
+        bool s3 = false;
+        try {
+            locked = PlayerPrefsExt.GetBool(level + "Locked");
+            s1 = PlayerPrefsExt.GetBool(level + "Star1");
+            s2 = PlayerPrefsExt.GetBool(level + "Star2");
+            s3 = PlayerPrefsExt.GetBool(level + "Star3");
+        }
+        catch (Exception ex) {
+            Debug.LogError("No key in player prefs: " + ex.Message);
+        }
 
+        LevelInfo info = new LevelInfo (index, s1, s2, s3, locked);
 		return info;
 	}
 	
 }
 
 public class LevelInfo {
-	public string name;
+    public int index;
+
 	public bool star1;
 	public bool star2;
 	public bool star3;
 	public bool locked;
 
-	public LevelInfo() {
-	
+
+	public LevelInfo(int i, bool s1, bool s2, bool s3, bool l) {
+        index = i;
+
+        star1 = s1;
+        star2 = s2;
+        star3 = s3;
+
+        locked = l;
 	}
 }
