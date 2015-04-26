c5bf5073e00b93edd66bc3168d528e95cc7a2261
jesseve <jesseverainen@gmail.com>
2015-04-26 14:06:30 +0300

    Kopterin valinta

---
+++ HeliResc/Assets/Scripts/CopterSelection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CopterSelection : MonoBehaviour {
+
+    private GameManager gameManager;
+
+    public GridLayoutGroup group;
+    public GameObject copterEntry;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+
+        RectTransform t = transform as RectTransform;       
+        group.cellSize = new Vector2(t.rect.width - 10, group.cellSize.y);
+
+        for(int i = 0; i < gameManager.CopterAmount; i++) {
+            GameObject go = Instantiate(copterEntry) as GameObject;
+            go.transform.SetParent(group.transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
