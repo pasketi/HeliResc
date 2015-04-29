22a166cb7c9510c698ba1cdbf57a9415dab4d131
Pasketi <pasketi@gmail.com>
2015-04-29 17:45:52 +0300

    ResetScript (Used last friday)

---
+++ HeliResc/Assets/Scripts/SetReset.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SetReset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Button> ().onClick.AddListener(() => GameObject.Find ("GameManager").GetComponent<GameManager>().resetData());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
