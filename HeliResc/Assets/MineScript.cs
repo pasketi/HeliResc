320e4028990d59a43a930d294b26459e056bc854
unknown <jesseve@ETY-B214-PR05.edu.wks.metropolia.fi>
2015-06-25 17:21:01 +0300

    miina

---
+++ HeliResc/Assets/MineScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineScript : MonoBehaviour {
+    
+    public GameObject explosion;    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("CrushBall"))
+        {            
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {        
+        GetComponent<SpriteRenderer>().enabled = false;
+        foreach (Collider2D c in GetComponents<Collider2D>())
+            c.enabled = false;
+        ParticleSystem[] parts = GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem p in parts)
+        {
+            p.Play();
+        }
+        explosion.SetActive(true);
+    }
+}
