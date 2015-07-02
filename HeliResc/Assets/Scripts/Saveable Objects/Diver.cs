17fd541f295f0eea25584141d9bc03e2a4c3c5eb
unknown <jesseve@ETY-B214-PR05.edu.wks.metropolia.fi>
2015-07-02 18:30:19 +0300

    sukeltaja

--- HeliResc/Assets/Scripts/Saveable Objects/Diver.cs
+++ HeliResc/Assets/Scripts/Saveable Objects/Diver.cs
@@ -3,5 +3,22 @@ using System.Collections;
 
 public class Diver : SaveableObject {
 
-	
+    protected Animator animator;
+
+    protected override void Start() {
+        base.Start();
+        animator = GetComponent<Animator>();
+    }
+
+    public override void GrabHook(Rigidbody2D hookRb) {
+        base.GrabHook(hookRb);
+
+        animator.Play("Hooked");
+    }
+
+    public override void DetachHook() {
+        base.DetachHook();
+
+        animator.Play("Diver");
+    }
 }
