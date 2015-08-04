5b0ce506070953fb191a49a3f4e7541414036dc0
unknown <ertes@ETY-B214-PR04.edu.wks.metropolia.fi>
2015-08-04 16:57:15 +0300

    MiniCopter and JumboCopter

---
+++ HeliResc/Assets/Scripts/Copter/MiniCopter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniCopter : Copter {
+	public CopterWaterManager waterManager;
+
+	protected override void AddUpgradables() {
+		base.AddUpgradables();
+		waterManager.Init(this);
+	}
+
+}
