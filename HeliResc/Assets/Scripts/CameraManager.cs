4bab483952002312077d946072bb1e465e0f7c2e
SoDream <erte345@gmail.com>
2015-08-27 19:56:34 +0300

    new camera y-moving

--- HeliResc/Assets/Scripts/CameraManager.cs
+++ HeliResc/Assets/Scripts/CameraManager.cs
@@ -10,7 +10,8 @@ public class CameraManager : MonoBehaviour {
 	public float 	deadZonePercent = 0.2f, 
 	dampTime = 2f, //good value is 2f
 	maxSpeed = 20f, //good value is 20f
-	maxY = 30f,
+	maxY = 30f, //the camera can not get higher than this
+	minY = 4f, //the camera can not get lower than this
 	mapBoundsLeft = 0f,
 	mapBoundsRight = 0f;
 	
@@ -18,7 +19,8 @@ public class CameraManager : MonoBehaviour {
 	fastDamp = 0.35f, //short dump time needed when copter takes fast action
 	slowVelosity = 1f, //velocity that is concidered to be slow (use slowDump in lower speed)
 	fastVelosity = 4.5f, //velocity that is concidered to be fast (use fastDump with higher speed)
-	cameraFront = 3.5f; //how much camera is forvard from copter's x positon
+	cameraFront = 3.5f, //how much camera is forvard from copter's x positon
+	cameraDownLook = 2.5f; //if the copter is moving down the camera looks further to see the sea or the ground earlier. 
 	private bool cameraMovingRight = false, //true if the copter and the camera is moving the same direction, false if copter change its flying direction.
 	cameraMovingLeft = false;
 	private Rigidbody2D copterRb;		//Reference to the copters rigidbody to access its velocity
@@ -26,7 +28,7 @@ public class CameraManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		if (copter == null) copter = GameObject.Find("Copter");
-		cameraOriginal = Camera.main.transform.position;
+		cameraOriginal = Camera.main.transform.position; //this is mainly to same camera z-position
 		deadZonePixels = Screen.width * deadZonePercent;
 		
 		copterRb = copter.GetComponent<Rigidbody2D> ();
@@ -40,59 +42,80 @@ public class CameraManager : MonoBehaviour {
 		
 		
 		Vector2 vel = copterRb.velocity;
+
+		//camera do not follow the copter if the copter does not excist (eg. it is exploded)
+		if (copter != null) {
+			if (vel.y < 0) {
+				//copter is moving down, so look down
+				targetY = copter.transform.position.y - cameraDownLook;
+			} else {
+				//copter is not moving down
+				targetY = copter.transform.position.y;
+			}
+			if (targetY > maxY)
+				targetY = maxY; //do not move camera to the space, let us stay just on earth
+			if (targetY < minY)
+				targetY = minY; //no need to see deap sea fish while flying
+			target = new Vector3 (targetX, targetY, cameraOriginal.z);
+			/*
+			 * THIS IS OLD CODE TO MANAGE CAMERA Y-POSITION
+			if (copter.transform.position.y <= minY * 2) {
+				targetY = cameraOriginal.y;
+				target = new Vector3 (targetX, targetY, cameraOriginal.z);
+			} else if ((Camera.main.WorldToScreenPoint (copter.transform.position).y >= Screen.height - Screen.height * (deadZonePercent / 2) || Camera.main.WorldToScreenPoint (copter.transform.position).y <= Screen.height * deadZonePercent * 2.5f)) {
+				targetY = copter.transform.position.y;
+				if (targetY > maxY)
+					targetY = maxY; //do not move camera to the space, let us stay just on earth
+				target = new Vector3 (targetX, targetY, cameraOriginal.z);
+			}
+			*/
 		
-		if (copter != null && copter.transform.position.y <= cameraOriginal.y*2){
-			targetY = cameraOriginal.y;
-			target = new Vector3(targetX, targetY, cameraOriginal.z);
-		} else if (copter != null && (Camera.main.WorldToScreenPoint(copter.transform.position).y >= Screen.height - Screen.height*(deadZonePercent/2) || Camera.main.WorldToScreenPoint(copter.transform.position).y <= Screen.height*deadZonePercent*2.5f)){
-			targetY = copter.transform.position.y;
-			if (targetY > maxY) targetY = maxY;
-			target = new Vector3(targetX, targetY, cameraOriginal.z);
-		}
-		//does copter enter in edge zone:
-		//left edge zone?
-		if (copter != null && (Camera.main.WorldToScreenPoint (copter.transform.position).x <= deadZonePixels)) {
-			cameraMovingLeft = true;
-		}
-		//Right edge zone?
-		if (copter != null && (Camera.main.WorldToScreenPoint (copter.transform.position).x >= (Screen.width - deadZonePixels))) {
-			cameraMovingRight = true;
-		}
-		//Should camera be moved?
-		if (copter != null && (cameraMovingLeft == true || cameraMovingRight == true)){
-			if (cameraMovingLeft == true) { 
-				targetX = (copter.transform.position.x - cameraFront); //Move camera so that player can see move left
-			}else{
-				targetX = (copter.transform.position.x + cameraFront); //Move camera so that player can see move right
+			//does copter enter in edge zone:
+			//left edge zone?
+			if (Camera.main.WorldToScreenPoint (copter.transform.position).x <= deadZonePixels) {
+				cameraMovingLeft = true;
 			}
-			//targetX = copter.transform.position.x; //move camera x position to copter's x position
-			if (hasBounds && targetX < mapBoundsLeft) targetX = mapBoundsLeft;
-			else if (hasBounds && targetX > mapBoundsRight) targetX = mapBoundsRight;
-			target = new Vector3(targetX, targetY, cameraOriginal.z);
-			
-			//Has copter changed its direction?
-			if (vel.x < 0 && cameraMovingRight == true) {
-				cameraMovingRight = false; //Stop moving wrong way
+			//Right edge zone?
+			if (Camera.main.WorldToScreenPoint (copter.transform.position).x >= (Screen.width - deadZonePixels)) {
+				cameraMovingRight = true;
 			}
-			if (vel.x > 0 && cameraMovingLeft == true) {
-				cameraMovingLeft = false; //Stop moving wrong way
-			}
-			//Should camera be moved fast?
-			if (Mathf.Abs(vel.x) > slowVelosity) {
-				if (Mathf.Abs(vel.x) > fastVelosity) {
-				//Move camera fast to get to the copter
-				dampTime = fastDamp;
-				}else{
-					//copter is moving within mid range velosity
-					//get suitable dampTime
-					dampTime = fastDamp+((slowDamp-fastDamp)*(1-((Mathf.Abs(vel.x)-slowVelosity)/(fastVelosity-slowVelosity))));
+			//Should camera be moved?
+			if (cameraMovingLeft == true || cameraMovingRight == true) {
+				if (cameraMovingLeft == true) { 
+					targetX = (copter.transform.position.x - cameraFront); //Move camera so that player can see move left
+				} else {
+					targetX = (copter.transform.position.x + cameraFront); //Move camera so that player can see move right
 				}
-			}else{
-				//the copter nearly moves. No need to hurry with the camera
-				dampTime = slowDamp;
+				//targetX = copter.transform.position.x; //move camera x position to copter's x position
+				if (hasBounds && targetX < mapBoundsLeft)
+					targetX = mapBoundsLeft;
+				else if (hasBounds && targetX > mapBoundsRight)
+					targetX = mapBoundsRight;
+				target = new Vector3 (targetX, targetY, cameraOriginal.z);
+			
+				//Has copter changed its direction?
+				if (vel.x < 0 && cameraMovingRight == true) {
+					cameraMovingRight = false; //Stop moving wrong way
+				}
+				if (vel.x > 0 && cameraMovingLeft == true) {
+					cameraMovingLeft = false; //Stop moving wrong way
+				}
+				//Should camera be moved fast?
+				if (Mathf.Abs (vel.x) > slowVelosity) {
+					if (Mathf.Abs (vel.x) > fastVelosity) {
+						//Move camera fast to get to the copter
+						dampTime = fastDamp;
+					} else {
+						//copter is moving within mid range velosity
+						//get suitable dampTime
+						dampTime = fastDamp + ((slowDamp - fastDamp) * (1 - ((Mathf.Abs (vel.x) - slowVelosity) / (fastVelosity - slowVelosity))));
+					}
+				} else {
+					//the copter nearly moves. No need to hurry with the camera
+					dampTime = slowDamp;
+				}
 			}
 		}
-		
 		if (Camera.main.transform.position.x != target.x || Camera.main.transform.position.y != target.y) gameObject.transform.position = Vector3.SmoothDamp(Camera.main.transform.position, target, ref zero, dampTime, maxSpeed);
 		//gameObject.transform.position = Vector3.Lerp (gameObject.transform.position, target, 3.5f * Time.deltaTime);
 		//gameObject.transform.position = Vector3.SmoothDamp(Camera.main.transform.position, target, ref zero, dampTime, maxSpeed); 
