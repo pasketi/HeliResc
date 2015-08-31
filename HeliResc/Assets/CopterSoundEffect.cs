6f54616c5c0b592f457d0ea4c7d136c13a7e8cfb
jesseve <jesseverainen@gmail.com>
2015-08-31 19:25:48 +0300

    Added music

--- HeliResc/Assets/CopterSoundEffect.cs
+++ HeliResc/Assets/CopterSoundEffect.cs
@@ -3,19 +3,30 @@ using System.Collections;
 
 public class CopterSoundEffect : MonoBehaviour {
 
+    public float minPitch = 0.75f;
+    public float maxPitch = 1.5f;
+    
     private AudioSource _audio;
+    private Copter copter;
+
+    private float power;
 
 	// Use this for initialization
-	void Awake () {
+	void Start () {
+        copter = GetComponent<Copter>();
         _audio = GetComponent<AudioSource>();
         Mute(SoundMusic.SoundMuted);
 	}
+
+    void Update() { 
+        _audio.pitch = Mathf.Clamp((maxPitch * copter.engine.CurrentPowerPercentage), minPitch, maxPitch);
+    }
 
     public void Mute(bool mute)
     {
         if (_audio == null)
             _audio = GetComponent<AudioSource>();
-        _audio.volume = mute ? 1 : 0;
+        _audio.mute = mute;
     }
 
     public void PlaySound()
