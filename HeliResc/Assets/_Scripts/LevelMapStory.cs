ca2ad320cdfaa08917a707e4d1a7e0148afffb90
pasketi <pasketi@gmail.com>
2017-02-05 19:57:59 +0200

    I think I'm going deaf! I think I'm going blind!

---
+++ HeliResc/Assets/_Scripts/LevelMapStory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMapStory : MonoBehaviour {
+
+    public GameObject StoryPanel;
+
+    void Awake()
+    {
+        hideStory();
+    }
+
+    public void hideStory()
+    {
+        StoryPanel.SetActive(false);
+    }
+
+    public void toggleStory()
+    {
+        if (!StoryPanel.active)
+        {
+            StoryPanel.SetActive(true);
+        }
+        else
+        {
+            StoryPanel.SetActive(false);
+        }
+    }
+
+}
